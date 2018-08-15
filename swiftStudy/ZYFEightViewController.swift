b3f4214ea3ac4574dd6c06dee6bcf09163c52256
张亚峰 <zhangyafeng@zhangyafengdemacbook-air.local>
2018-08-15 23:13:44 +0800

    重载构造函数

--- swiftStudy/ZYFEightViewController.swift
+++ swiftStudy/ZYFEightViewController.swift
@@ -16,7 +16,8 @@ class ZYFEightViewController: UIViewController {
         let p = ZYFStudent()
         print(p.name + p.no)
         
-        
+        let p1 = ZYFPerson(name: "fengfeng111")
+        print(p1)
     }
 
 
--- swiftStudy/ZYFPerson.swift
+++ swiftStudy/ZYFPerson.swift
@@ -8,16 +8,33 @@
 
 import UIKit
 
+/**
+ 1.给自己的属性分配空间并且设置初始值
+ 2.调用父类的构造函数之前，需要先给本类额的属性设置初始值
+ 3.调用父类的·构造函数·，给弗雷的属性分配空间设置初始值
+    NSObject 没有属性， 只有一个成员变量 ‘isa’
+ 4.如果重载了构造函数，并且没有实现父类 init 方法，系统不再提供 init() 构造函数(默认是会有的!)
+ */
 class ZYFPerson: NSObject {
     var name: String
-    
+    //重写
     override init() {
+        
         print("person init")
+        
         //分配空间设置初始值
         name = "fengfeng"
         
         super.init()
         
+    }
+    //重载，函数名相同，但是参数和个数不同
+    init(name: String) {
+        //使用参数的name赋值给属性
+        self.name = name
+
+        //调用父类的构造寒素
+        super.init()
     }
     
     
